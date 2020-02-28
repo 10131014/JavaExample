28f288361fd065407fa3d442f79b22ca3521b736
10131014 <1249437885@qq.com>
2020-02-28 21:10:10 +0800

    算法题目练习

---
+++ src/NormalAlgorithm/isSubsequence/Solution.java
@@ -0,0 +1,23 @@
+package NormalAlgorithm.isSubsequence;
+
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length()==0)
+        {
+            return true;
+        }
+        int j=0;
+        for (int i=0;i<t.length();i++)
+        {
+            if (s.charAt(j)==t.charAt(i))
+            {
+                j++;
+                if (j==(s.length()))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
