b4fcf6f122ba8d72f7db2dfb5ffa639a7c69b84f
10131014 <1249437885@qq.com>
2020-09-16 10:51:46 +0800

    算法题目练习

---
+++ src/DynamicProgrammingAlgorithm/maxValue/Solution.java
@@ -0,0 +1,23 @@
+package DynamicProgrammingAlgorithm.maxValue;
+//剑指offer 47. 礼物的最大价值
+//动态规划
+public class Solution {
+    public int maxValue(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int j = 1; j < m; j++) {
+            dp[j][0] = dp[j - 1][0] + grid[j][0];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
